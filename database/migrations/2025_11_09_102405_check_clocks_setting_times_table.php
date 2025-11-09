ce8fbe8b99e747fb70ff56bab9dfc75246c18f72
Audric Andhika <audricc015@gmail.com>
2025-11-09 17:41:46 +0700

    feat: add migrations

---
+++ database/migrations/2025_11_09_102405_check_clocks_setting_times_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('check_clocks_settings_times', function (Blueprint $table) {
+                $table->id('id');
+                $table->unsignedBigInteger('ck_settings_id');
+                $table->date('day');
+                $table->time('clock_in');
+                $table->time('clock_out');
+                $table->time('break_start');
+                $table->time('break_end');
+                $table->timestamp('created_at')->useCurrent();
+                $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+                $table->timestamp('deleted_at')->nullable();
+
+
+                $table->foreign('ck_settings_id')->references('id')->on('check_clocks_settings')->onDelete('cascade');
+
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
